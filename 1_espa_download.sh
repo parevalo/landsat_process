bc4477682dc333a30339ce12fec206cb1f51df84
parevalo <parevalo@bu.edu>
2016-09-08 10:21:03 -0400

    Update script to reflect changes in bulk downloader

--- 1_espa_download.sh
+++ 1_espa_download.sh
@@ -4,14 +4,15 @@
 #$ -N dwnl_multi
 #$ -j y
 #$ -m e
+#$ -b y
 
 module load python/2.7.5_nopath
 
 EMAIL=$1
 ORDER=$2
 DIR=$3
+USR=$4
+PWD=$5
 
-
-python ~/Scripts/landsat_process/bulk-downloader/download_espa_order.py -e $EMAIL \
-	-o $ORDER -c \
-	-d $DIR
+python ~/Scripts/landsat_process/espa-bulk-downloader/download_espa_order.py -e $EMAIL \
+	-o $ORDER -d $DIR -u $USR -p $PWD
